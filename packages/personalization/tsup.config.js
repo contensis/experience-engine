d530837cfaec546b9abf188f3f014fc09b12feb9
Neil Flatley <n.flatley@zengenti.com>
2024-12-18 17:22:34 +0000

    build: bundled sourcemaps are incorrect when debugging

--- packages/personalization/tsup.config.js
+++ packages/personalization/tsup.config.js
@@ -9,6 +9,6 @@ export default defineConfig({
   // this runs tsc to do type checking during build but the console
   // output is not as readable as vanilla tsc when there are problems to resolve
   dts: true,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
 });
