04ac70e59710eda9cc2dbedc81f965ece4b9ef71
Neil Flatley <n.flatley@zengenti.com>
2025-01-03 15:43:40 +0000

    build: add minified bundles in package builds

--- packages/personalization/tsup.config.js
+++ packages/personalization/tsup.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: {
-    ["personalization"]: "src/index.ts",
-  },
-  format: ["esm", "cjs"],
-  target: "es6",
-  // this runs tsc to do type checking during build but the console
-  // output is not as readable as vanilla tsc when there are problems to resolve
-  dts: true,
-  sourcemap: false,
-  clean: true,
+export default defineConfig((options) => {
+  const outputName = `personalization${options.minify ? ".min" : ""}`;
+  const oneTime = !options.minify;
+
+  return {
+    entry: {
+      [outputName]: "src/index.ts",
+    },
+    // splitting: true,
+    // outDir: "dist",
+    format: ["esm", "cjs"],
+    target: "es6",
+    // this runs tsc to do type checking during build but the console
+    // output is not as readable as vanilla tsc when there are problems to resolve
+    dts: oneTime,
+    sourcemap: false,
+    clean: oneTime,
+    minify: options.minify,
+  };
 });
--- packages/react/tsup.config.js
+++ packages/react/tsup.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: {
-    ["personalization-react"]: "src/index.tsx",
-  },
-  format: ["esm", "cjs"],
-  target: "es6",
-  // this runs tsc to do type checking during build but the console
-  // output is not as readable as vanilla tsc when there are problems to resolve
-  dts: true,
-  sourcemap: true,
-  clean: true,
+export default defineConfig((options) => {
+  const outputName = `personalization-react${options.minify ? ".min" : ""}`;
+  const oneTime = !options.minify;
+  return {
+    entry: {
+      [outputName]: "src/index.tsx",
+    },
+    format: ["esm", "cjs"],
+    target: "es6",
+    // this runs tsc to do type checking during build but the console
+    // output is not as readable as vanilla tsc when there are problems to resolve
+    dts: oneTime,
+    sourcemap: oneTime,
+    clean: oneTime,
+    minify: options.minify,
+  };
 });
