b427a423a914761cbe1afa5e19cfa571a495bee4
Neil Flatley <n.flatley@zengenti.com>
2025-04-23 16:24:41 +0100

    test: add further examples to standalone html app

--- apps/html/personalization.js
+++ apps/html/personalization.js
@@ -2,11 +2,9 @@
 const scriptEl = document.createElement("script");
 
 /** Set data attributes to provide options to create the personalisation context */
-scriptEl.setAttribute(
-  "data-contensis-personalization-root-url",
-  "https://personalization-api-contensis-dev.services.contensis.com"
-);
+scriptEl.setAttribute("data-contensis-personalization-alias", "develop");
 scriptEl.setAttribute("data-contensis-personalization-project-id", "website");
+scriptEl.setAttribute("data-contensis-personalization-preview", "true");
 scriptEl.setAttribute("data-contensis-personalization-debug", "true");
 scriptEl.setAttribute(
   "src",
